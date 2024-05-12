bb51608401bfaafbf9b0559163c1abebe20ad657
DereWah <daviloca03@gmail.com>
2024-05-12 09:21:07 +0200

    fix: regex pattern change for version compatibility

--- src/main/java/org/derewah/skriptanvilgui/SkriptAnvilGUI.java
+++ src/main/java/org/derewah/skriptanvilgui/SkriptAnvilGUI.java
@@ -61,17 +61,16 @@ public class SkriptAnvilGUI extends JavaPlugin {
         Bukkit.getLogger().info("[Skript-AnvilGUI] has been enabled!");
     }
 
-    public boolean isCompatible(String version, String limit) {
+    public static boolean isCompatible(String version, String limit) {
         // Regular expression pattern to extract version number
-        String regex = "\\(MC: ([0-9.]+)\\)";
+        Pattern versionPattern = Pattern.compile("(?i)\\(MC: (\\d)\\.(\\d+)\\.?(\\d+?)?(?: (Pre-Release|Release Candidate) )?(\\d)?\\)");
 
         // Compile the pattern
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(version);
+        Matcher matcher = versionPattern.matcher(version);
 
         // Check if the pattern is found in the input string
         if (matcher.find()) {
-            String versionString = matcher.group(1);
+            String versionString = matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3);
             // Split the version string by dots
             String[] versionNumbers = versionString.split("\\.");
             String[] limitNumbers = limit.split("\\.");
